bdd53ac632496cbe20c703116d0512eeb2e6de4e
Jyotika Purohit <emailjyotikapurohit@gmail.com>
2020-08-26 17:01:44 +0530

    delete likes of post/comment when deleting post/comment

--- controllers/comments_controller.js
+++ controllers/comments_controller.js
@@ -1,11 +1,10 @@
 const Comment=require('../models/comment');
 const Post=require('../models/post');
 const User=require('../models/user');
+const Like=require('../models/likes');
 
 module.exports.create =async function(req,res){
     try{
-        
-
         let comment=await Comment.create({
             content:req.body.comment_content,
             post:req.params.post_id,
@@ -29,8 +28,13 @@ module.exports.delete=async function(req,res){
     try{
         let comment= await Comment.findById(req.params.comment_id).populate('user');
         let post=await Post.findById(comment.post).populate('user');
-       
+        
         if(req.user.id == comment.user.id || req.user.id == post.user.id){
+            await Like.deleteMany({
+                onModel:'Comment',
+                likeable:req.params.comment_id
+            });
+
             let post=await Post.findByIdAndUpdate(comment.post,{$pull :{comments:req.params.comment_id}});
             post.save();
             comment.remove();
--- controllers/posts_controller.js
+++ controllers/posts_controller.js
@@ -1,5 +1,7 @@
 const Post=require('../models/post');
 const User=require('../models/user');
+const Comment=require('../models/comment');
+const Like=require('../models/likes');
 
 module.exports.create=async function(req,res){
     try{
@@ -20,7 +22,11 @@ module.exports.create=async function(req,res){
 
 module.exports.deletePost=async function(req,res){
      try{
-        let post=await Post.findByIdAndDelete(req.params.post_id);
+        let post=await Post.findById(req.params.post_id);
+        await Like.deleteMany({likeable:{$in:post.comments}});
+        await Like.deleteMany({likeable:req.params.post_id});
+        await Comment.deleteMany({post:req.params.post_id});
+        post.remove();
         req.flash('success','Post deleted');
         return res.redirect('back');
      }catch(error){
