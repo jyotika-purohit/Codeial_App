3f0755f5836c9b6e6c9defe3bacdb37a23501464
Jyotika Purohit <emailjyotikapurohit@gmail.com>
2020-09-02 17:38:17 +0530

    setup the environment variables in system and also define the production keys using those env variables and few changes in package.json for starting the prod server

--- config/environment.js
+++ config/environment.js
@@ -19,8 +19,28 @@ const development = {
     google_client_Secret:'9U2ocpRj0OBiHFxKHPARl_yG',
     google_callback_URL:'http://localhost:8000/users/auth/google/callback'
 }
+
 const production = {
-    name:'production'
+    name:'production',
+    asset_path : process.env.CODEIAL_ASSET_PATH,
+    session_cookie_key :process.env.CODEIAL_SESSION_COOKIE_KEY,
+    db_name : process.env.CODEIAL_DB,
+    smtp:{
+        host: "smtp.gmail.com",
+        port: 587,
+        secure: false, // true for 465, false for other ports
+        auth: {
+          user: process.env.CODEIAL_GMAIL_USERNAME, // generated ethereal user
+          pass: process.env.CODEIAL_GMAIL_PASSWORD, // generated ethereal password
+        },
+        tls: {
+            rejectUnauthorized: false
+        }
+    },
+    google_client_ID:process.env.CODEIAL_GOOGLE_CLIENT_ID,
+    google_client_Secret:process.env.CODEIAL_GOOGLE_CLIENT_SECRET,
+    google_callback_URL:process.env.CODEIAL_GOOGLE_CALLBACK_URL
 }
 
-module.exports=development;
+module.exports=eval(process.env.CODEIAL_ENVIRONMENT) == undefined ? development : eval(process.env.CODEIAL_ENVIRONMENT);
+//if process.env.CODEIAL_ENVIRONMENT is udinfed then it's development
